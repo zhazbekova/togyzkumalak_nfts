c893b890de0d116f5f26f087fe53bb21796c7bce
zhazbekova <ulpanzhazbekova@gmail.com>
2023-06-02 09:55:17 +0600

    first commit

---
+++ my-app/pages/api/[tokenId].js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+    // get the tokenId from the query params
+    const tokenId = req.query.tokenId;
+    // As all the images are uploaded on github, we can extract the images from github directly.
+      const image_url ='https://gateway.pinata.cloud/ipfs/QmfESSTwrrhh9HVZdkiqhaSPMXQSa4M1Loa5q3aDBWwYBx/';
+    // The api is sending back metadata for a Crypto Dev
+    // To make our collection compatible with Opensea, we need to follow some Metadata standards
+    // when sending back the response from the api
+    // More info can be found here: https://docs.opensea.io/docs/metadata-standards
+    res.status(200).json({
+      name: "TogyzKumalak #" + tokenId,
+      description: "NFT Collection",
+      image: image_url + tokenId + ".png",
+    });
+  }
+    
+   
