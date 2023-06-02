bc2ce0723ee4838215529ad06034539c08a22dfc
zhazbekova <ulpanzhazbekova@gmail.com>
2023-06-02 12:24:27 +0600

    2nd commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x9d4E8D4b5c77B52fB7772b3cdA9F26df0CFC35a1";
+//const WHITELIST_CONTRACT_ADDRESS = "0x9d4E8D4b5c77B52fB7772b3cdA9F26df0CFC35a1";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://togyzkumalak-nfts.vercel.app/api/";
 
