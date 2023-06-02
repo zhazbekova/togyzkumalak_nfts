6837617f20f3491cff203b08f60c4a531c0be742
zhazbekova <ulpanzhazbekova@gmail.com>
2023-06-02 10:23:33 +0600

    changed image url

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+      "https://raw.githubusercontent.com/zhazbekova/togyzkumalak_nfts/main/my-app/public/nfts/1%20(1).svg?token=GHSAT0AAAAAACBRJODOAE3NL3XSRH4FXESGZDZPSVQ";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
