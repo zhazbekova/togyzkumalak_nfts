ae6981925cab7633ffafb41804dd21775a2aa818
zhazbekova <ulpanzhazbekova@gmail.com>
2023-06-02 10:20:03 +0600

    img

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -2,7 +2,8 @@ export default function handler(req, res) {
     // get the tokenId from the query params
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
-      const image_url ='https://gateway.pinata.cloud/ipfs/QmfESSTwrrhh9HVZdkiqhaSPMXQSa4M1Loa5q3aDBWwYBx/';
+    const image_url =
+      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
@@ -10,8 +11,6 @@ export default function handler(req, res) {
     res.status(200).json({
       name: "TogyzKumalak #" + tokenId,
       description: "NFT Collection",
-      image: image_url + tokenId + ".png",
+      image: image_url + tokenId + ".svg",
     });
   }
-    
-   
